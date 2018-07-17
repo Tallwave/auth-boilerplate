52f566007dbc083a100a2933f3359e6f37a5dc10
tinaheiligers <tinaheiligers@gmail.com>
2018-07-17 16:36:11 -0700

    Refactors jwt methods with responses

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,31 +1,32 @@
 const jwt = require('jsonwebtoken');
 
-const signJWT = (user) => {
-  return jwt.sign({
-    exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),// setting expiration as 1 day
-    id: user.id,
-    username: user.username,
-  }, process.env.JWT_PASSWORD);
-};
-
-const setJWTAndRespond = (user, h, url) => {
-  const jwtToken = signJWT(user);
-  const response = h.response({ token: jwtToken })
-    .state('session', jwtToken)
-    .header('Authorization', `jwt ${jwtToken}`)
-    .header('Access-Control-Allow-Credentials', true);
-  if (url && url !== '') {
-    response.redirect(url);
-  }
-  return response;
-};
-const setJWTAndRespondNoRedirect = (user, h) => {
-  const userJWTtoken = signJWT(user);
-  const response = h.response({ token: userJWTtoken })
-    .state('session', userJWTtoken)
-    .header('Authorization', `jwt ${userJWTtoken}`)
-    .header('Access-Control-Allow-Credentials', true);
-  return response;
-};
-
-module.exports = [{ setJWTAndRespond, setJWTAndRespondNoRedirect }];
+module.exports =
+  {
+    setJWTAndRespond: (user, h, url) => {
+      const jwtToken = jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),// setting expiration as 1 day
+        id: user.id,
+        username: user.username,
+      }, process.env.JWT_PASSWORD);
+      const response = h.response({ token: jwtToken })
+        .state('session', jwtToken)
+        .header('Authorization', `jwt ${jwtToken}`)
+        .header('Access-Control-Allow-Credentials', true);
+      if (url && url !== '') {
+        response.redirect(url);
+      }
+      return response;
+    },
+    setJWTAndRespondNoRedirect: (user, h) => {
+      const userJWTtoken = jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),// setting expiration as 1 day
+        id: user.id,
+        username: user.username,
+      }, process.env.JWT_PASSWORD);
+      const response = h.response({ token: userJWTtoken })
+        .state('session', userJWTtoken)
+        .header('Authorization', `jwt ${userJWTtoken}`)
+        .header('Access-Control-Allow-Credentials', true);
+      return response;
+    },
+  };
