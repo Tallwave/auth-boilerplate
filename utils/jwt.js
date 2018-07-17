9ef8ab0ddf38e91a21646a152215aa4e9ca1891c
tinaheiligers <tinaheiligers@gmail.com>
2018-07-17 15:52:25 -0700

    Extracts JWT creation

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const setJWTUserToken = (user) => {
+const setJWTSessionCookie = (user) => {
   return jwt.sign({
     exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),// setting expiration as 1 day
     id: user.id,
@@ -9,15 +9,10 @@ const setJWTUserToken = (user) => {
 };
 
 const setJWTAndRespond = (user, h, url) => {
-  const userJWTtoken = jwt.sign({
-    exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),// setting expiration as 1 day
-    id: user.id,
-    username: user.username,
-  }, process.env.JWT_PASSWORD);
-
-  const response = h.response({ token: userJWTtoken })
-    .state('session', userJWTtoken)
-    .header('Authorization', `jwt ${userJWTtoken}`)
+  const jwtToken = setJWTSessionCookie(user);
+  const response = h.response({ token: jwtToken })
+    .state('session', jwtToken)
+    .header('Authorization', `jwt ${jwtToken}`)
     .header('Access-Control-Allow-Credentials', true);
   if (url && url !== '') {
     response.redirect(url);
@@ -25,7 +20,7 @@ const setJWTAndRespond = (user, h, url) => {
   return response;
 };
 const setJWTAndRespondNoRedirect = (user, h) => {
-  const userJWTtoken = setJWTUserToken(user);
+  const userJWTtoken = setJWTSessionCookie(user);
   const response = h.response({ token: userJWTtoken })
     .state('session', userJWTtoken)
     .header('Authorization', `jwt ${userJWTtoken}`)
